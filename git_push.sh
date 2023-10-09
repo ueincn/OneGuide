42a85a784e8b1141d88be75b4c6aa0bfa046ec94
fufouaka <fufouaka@163.com>
2023-10-09 19:29:26 +0800

    ueincn

---
+++ git_push.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+git add *
+echo ""
+
+git commit -m "ueincn"
+echo ""
+
+git push
+
+echo ""
+echo "=========================="
+if [ $? == "0" ]; then
+	echo "Git Push Complete!"
+else
+	echo "Git Push Fail!"
+fi
